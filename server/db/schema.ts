f433ca1de8c977f9fc0f3ed185243f6a124da3a5
John Nguyen <johnnguyen4501@gmail.com>
2025-09-17 03:16:58 -0400

    fix schema primary key

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,13 +1,7 @@
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 
-// export const usersTable = pgTable('users', {
-//   id: text().primaryKey(),
-//   email: text().notNull(),
-//   created_at: timestamp().defaultNow().notNull(),
-// })
-
 export const m3u8CacheTable = pgTable('m3u8_cache', {
   slug: text().primaryKey(),
-  url: text().notNull().unique(),
+  url: text().notNull(),
   created_at: timestamp().defaultNow().notNull(),
 })
